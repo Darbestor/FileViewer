27bfdd4c4ad8f2f7f1c2c627a945fbe60a09326e
Darbe <fordarbe@gmail.com>
2019-12-14 14:30:24 +0300

    Open and read text

--- FileViewer/MainWindow.xaml.cs
+++ FileViewer/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,18 @@ namespace FileViewer
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+
+        private void txtEditor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
